9d0f439f2accf2b9406df77309e4f1147bdd12e8
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2025-10-06 20:20:26 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  serverBasePath: 'https://restyle-web-services-cyf0axfvakcxaehd.brazilsouth-01.azurewebsites.net/api/v1'
+  serverBasePath: 'https://restyle-platform-bed4c3b3f3eug0ak.canadacentral-01.azurewebsites.net/api/v1'
 }
